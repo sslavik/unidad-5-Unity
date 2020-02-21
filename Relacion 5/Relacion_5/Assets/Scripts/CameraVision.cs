d98404b29500bf8f40c6e4b7717fa4d1bd8b29e0
sslavik <sslavikshy@gmail.com>
2020-02-21 12:45:43 +0100

    Added modifications to Relacion 5 Menu

---
+++ Relacion 5/Relacion_5/Assets/Scripts/CameraVision.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraVision : MonoBehaviour
+{
+    RaycastHit hited;
+    bool somethingHitted = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        somethingHitted = Physics.BoxCast(transform.position,new Vector3(Camera.main.pixelWidth, Camera.main.pixelHeight, 100), transform.forward, out hited);
+    }
+
+    private void OnDrawGizmos() {
+        if(somethingHitted)
+            Gizmos.DrawCube(transform.position, transform.localScale);
+        Gizmos.DrawCube(transform.position, transform.localScale);
+    }
+}
